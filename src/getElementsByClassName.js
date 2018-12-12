6e87a25f292df7d60e3586b84c85def94b7bdaff
Chris Tso <christophertso2017@u.northwestern.edu>
2018-12-11 19:19:56 -0600

    Passed most of object cases

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,27 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
+	var newnodes = [];
+	var Array2 = [];
+	var nodes = document.body.childNodes;
+	for(var i  = 0; i < nodes.length; i++){
+		var nodelist = nodes[i].classList;
+		//add those with classlist
+		if(nodelist !== undefined){
+			newnodes.push(nodes[i]);
+	    }
+	 }
+     for(var j = 0; j < newnodes.length; j++){
+     	for(var z = 0; z < newnodes[j].classList.length; z++){
+     		if(newnodes[j].classList[z] === className){
+     			Array2.push(newnodes[j]);
+     		}
+     	}
+     }    
+	    
+	
+ return Array2;	
+}
   // your code here
-};
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,92 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+    var string1 = '';
+ if(typeof obj === 'object' && !Array.isArray(obj) && obj !== null){
+  if((Object.keys(obj)).length === 0 ){
+    return "{}";
+  }
+  var keys1 = Object.keys(obj);
+  var values1 = Object.values(obj);
+  for(var i = 0; i < values1.length; i++){
+    if(i !== values1.length-1 && (values1[i] !== true && values1[i] !== false && values1[i] !== null)){
+      string1 +=  '"' + keys1[i] + '"' + ':' + '"' + values1[i] + '"' + ',';
+    }
+    else if(i !== values1.length-1 && (values1[i] == true || values1[i] == false || values1[i] == null)){
+      string1 +=  '"' + keys1[i] + '"' + ':' +  values1[i] + ',';
+    }
+
+    else if(i === values1.length-1 && (values1[i] !== true && values1[i] !== false && values1[i] !== null)){
+       string1 +=  '"' + keys1[i] + '"' + ':' + '"' + values1[i] + '"';
+    }
+    else if(i === values1.length- 1 && (values1[i] === true || values1[i] === false || values1[i] === null)){
+       string1 +=  '"' + keys1[i] + '"' + ':' +  values1[i];
+    }
+    
+  }
+return '{' + string1 + '}';
+}
+
+
+  if(Array.isArray(obj)){
+      for(var i = 0; i < obj.length; i++){
+        if(Array.isArray(obj[i]) && obj[i].length === 0){
+          string1 += stringifyJSON(obj[i]) + ','  ;
+        }
+        else if(Array.isArray(obj[i]) && obj[i].length !== 0){
+          string1 += stringifyJSON(obj[i]);
+        }
+        else{
+         if(i !== obj.length-1){
+           if(typeof obj[i] === 'string'){
+           string1 += '"' + obj[i] + '"';
+           }   
+          else if(typeof obj[i] !== 'string'){
+            string1 += obj[i] + ',';
+          }
+          
+
+         }
+
+         if(i === obj.length-1){
+           if(typeof obj[i] === 'string'){
+           string1 += '"' + obj[i] + '"';
+           } 
+           if(typeof obj[i] !== 'string'){
+           string1 +=  obj[i];
+           } 
+          }
+          
+        }   
+      }  
+     return '[' + string1 + ']'; 
+    } 
+      if(typeof obj !== 'string'){
+    string1 += obj;
+    return string1;
+  }
+   if(typeof obj === 'string'){
+     string1 += '"' + obj + '"';
+     return string1;
+   }
+}
+
+  /*var keys1 = Object.keys(obj);
+  var values1 = Object.values(obj);
+  
+  for(var i = 0; i < values1.length; i++){
+    if(i !== values1.length-1){
+    string1 +=  '"' + keys1[i] + '"' + ':' + '"' + values1[i] + '"' + + ', ';
+     }
+     if(i === values1.length-1){
+    string1 +=  '"' + keys1[i] + '"' + ':' + '"' + values1[i] + '"' ;
+     }
+  }
+  return '{' + string1 + '}';
+  }*/
+   
+ 
+
+ 
+
+
